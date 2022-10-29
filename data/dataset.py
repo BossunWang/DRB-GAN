b25aa702957738e24f5bbe1732e1b993fd38b319
Bossun Wang <vvmodouco@gmail.com>
2022-10-29 16:17:00 +0800

    modify dataset to load txt file

--- data/dataset.py
+++ data/dataset.py
@@ -26,10 +26,15 @@ def make_dataset(dir):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, data_root, transform, get_path=False):
-        self.data_root = data_root
-        self.train_list = []
-        self.files = make_dataset(self.data_root)
+    def __init__(self, data_file, transform, get_path=False):
+        if os.path.isdir(data_file):
+            self.files = make_dataset(data_file)
+        else:
+            self.files = []
+            f = open(data_file, 'r')
+            self.files = [line.strip() for line in f.readlines()]
+            f.close()
+
         self.transform = transform
         self.get_path = get_path
 
@@ -52,19 +57,22 @@ class ImageDataset(Dataset):
 
 
 class ImageClassDataset(Dataset):
-    def __init__(self, data_root, transform, sample_size=1):
+    def __init__(self, data_root, transform, sample_size=1, assigned_labels=[], assigned_transform=[]):
         self.data_root = data_root
         self.label_dict = os.listdir(data_root)
-        self.files = make_dataset(self.data_root)
+        self.files = make_dataset(data_root)
         self.transform = transform
         self.sample_size = sample_size
+        self.assigned_labels = assigned_labels
+        self.assigned_transform = assigned_transform
+        print("label dict:", self.label_dict)
 
     def __len__(self):
         return len(self.label_dict)
 
-    def to_Tensor(self, image_path):
+    def to_Tensor(self, image_path, transform):
         image = Image.open(image_path).convert('RGB')
-        image = self.transform(image)
+        image = transform(image)
         return image
 
     def __getitem__(self, index):
@@ -76,7 +84,14 @@ class ImageClassDataset(Dataset):
         label_list = []
         for fi in file_index_list:
             image_path = os.path.join(self.data_root, image_dir, files[fi])
-            image = self.to_Tensor(image_path)
+            if len(self.assigned_labels) > 0 and index in self.assigned_labels:
+                image = self.to_Tensor(image_path, self.assigned_transform[self.assigned_labels.index(index)])
+                # style_image = image.cpu().numpy().transpose(1, 2, 0)
+                # style_image = (style_image * std_array) + mean_array
+                # style_image = np.clip(style_image * 255.0, 0, 255)
+                # cv2.imwrite('sample_assigned_style.png', style_image)
+            else:
+                image = self.to_Tensor(image_path, self.transform)
             image_list.append(image)
             label_list.append(torch.Tensor([index]))
         return torch.cat(image_list), torch.cat(label_list).long()
@@ -91,8 +106,8 @@ if __name__ == '__main__':
     from tqdm import tqdm
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_size = 256
-    crop_size = 256
+    input_size = 512
+    crop_size = 512
     sample_isze = 3
     train_transform = [T.RandomHorizontalFlip()
                        , T.Resize(input_size, InterpolationMode.BICUBIC)
@@ -100,17 +115,24 @@ if __name__ == '__main__':
                        , T.ToTensor()
                        , T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]
     train_transform = T.Compose(train_transform)
+    train_assigned_transform = [T.RandomHorizontalFlip()
+                                , T.CenterCrop(crop_size)
+                                , T.ToTensor()
+                                , T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]
+    train_assigned_transform = T.Compose(train_assigned_transform)
     test_transform = [T.Resize(input_size, InterpolationMode.BICUBIC)
                       , T.ToTensor()
                       , T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]
     test_transform = T.Compose(test_transform)
 
-    train_data_src = ImageDataset('../../Filtered_Places365/train'
+    train_data_src = ImageDataset('train.txt'
                                   , train_transform)
-    train_data_tgt = ImageClassDataset('../../data_art_backup'
+    train_data_tgt = ImageClassDataset('../../art_dataset_v2'
                                        , train_transform
-                                       , sample_isze)
-    test_data_tgt = ImageDataset('../../Filtered_Places365/val/normal'
+                                       , sample_isze
+                                       , assigned_labels=[6]
+                                       , assigned_transform=[train_assigned_transform])
+    test_data_tgt = ImageDataset('test.txt'
                                  , test_transform)
 
     batch_size = 1
@@ -132,28 +154,28 @@ if __name__ == '__main__':
     mean_array = np.array((0.5, 0.5, 0.5)).reshape(1, 1, -1)
     std_array = np.array((0.5, 0.5, 0.5)).reshape(1, 1, -1)
 
-    # for content_image in tqdm(train_loader_src):
-    #     content_image = content_image.to(device)
-    #     content_image = content_image[0].cpu().numpy().transpose(1, 2, 0)
-    #
-    #     content_image = cv2.cvtColor(content_image, cv2.COLOR_BGR2RGB)
-    #     content_image = (content_image * std_array) + mean_array
-    #     content_image = np.clip(content_image * 255.0, 0, 255)
-    #     cv2.imwrite('sample_train_content.png', content_image)
-    #
-    #     try:
-    #         style_image_list, label_list = next(style_batch_iterator)
-    #     except StopIteration:
-    #         style_batch_iterator = iter(train_loader_tgt)
-    #         style_image_list, label_list = next(style_batch_iterator)
-    #
-    #     print("style_image_list:", style_image_list.size())
-    #     print("label_list:", label_list.size())
-    #
-    #     style_image = style_image_list[0, :3, :, :].cpu().numpy().transpose(1, 2, 0)
-    #     style_image = (style_image * std_array) + mean_array
-    #     style_image = np.clip(style_image * 255.0, 0, 255)
-    #     cv2.imwrite('sample_train_style.png', style_image)
+    for content_image in tqdm(train_loader_src):
+        content_image = content_image.to(device)
+        content_image = content_image[0].cpu().numpy().transpose(1, 2, 0)
+
+        content_image = cv2.cvtColor(content_image, cv2.COLOR_BGR2RGB)
+        content_image = (content_image * std_array) + mean_array
+        content_image = np.clip(content_image * 255.0, 0, 255)
+        cv2.imwrite('sample_train_content.png', content_image)
+
+        try:
+            style_image_list, label_list = next(style_batch_iterator)
+        except StopIteration:
+            style_batch_iterator = iter(train_loader_tgt)
+            style_image_list, label_list = next(style_batch_iterator)
+
+        # print("style_image_list:", style_image_list.size())
+        # print("label_list:", label_list.size())
+
+        style_image = style_image_list[0, :3, :, :].cpu().numpy().transpose(1, 2, 0)
+        style_image = (style_image * std_array) + mean_array
+        style_image = np.clip(style_image * 255.0, 0, 255)
+        cv2.imwrite('sample_train_style.png', style_image)
 
     for content_image in tqdm(test_loader_src):
         content_image = content_image.to(device)
